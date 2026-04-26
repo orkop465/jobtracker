b63fbfca26217f9377a4063770500d4a4cd63b62
orkop <orkop465@gmail.com>
2026-04-26 03:23:03 +0000

    fix: footer edit

--- components/landing/footer.tsx
+++ components/landing/footer.tsx
@@ -34,23 +34,21 @@ export function LandingFooter() {
               <li><a href="#stages" onClick={handleAnchorClick}>Pipeline</a></li>
               <li><a href="#analytics" onClick={handleAnchorClick}>Analytics</a></li>
               <li><a href="#marketplace" onClick={handleAnchorClick}>Resume market</a></li>
-              <li><a href="#">Roadmap</a></li>
             </ul>
           </div>
           <div className="footer-col">
             <h4>Resources</h4>
             <ul>
-              <li><a href="#">Changelog</a></li>
-              <li><a href="#">Comp database</a></li>
-              <li><a href="#">Templates</a></li>
-              <li><a href="#">Blog</a></li>
+              <li><a href="#">Temp</a></li>
+              <li><a href="#">Temp</a></li>
+              <li><a href="#">Temp</a></li>
+              <li><a href="#">Temp</a></li>
             </ul>
           </div>
           <div className="footer-col">
             <h4>Company</h4>
             <ul>
               <li><a href="#">About</a></li>
-              <li><a href="#">Careers</a></li>
               <li><a href="#">Privacy</a></li>
               <li><a href="#">Terms</a></li>
             </ul>
@@ -58,7 +56,7 @@ export function LandingFooter() {
         </div>
         <div className="footer-base">
           <span>{'\u00A9'} 2026 Maakavoda Labs {'\u2014'} Made for technical job hunters.</span>
-          <span className="mono" style={{ fontSize: '12px' }}>v1.4.2 {'\u00B7'} uptime 99.98%</span>
+          <span className="mono" style={{ fontSize: '12px' }}>v1.4.2</span>
         </div>
       </div>
     </footer>
